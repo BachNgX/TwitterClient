8a44f4caf8191274c923837e0ed6352d03752d45
Duanpham <duanphamhvbc@gmail.com>
2018-11-09 10:04:59 +0700

    @@

--- app/src/main/java/usth/edu/vn/twitterclient/SetupActivity.java
+++ app/src/main/java/usth/edu/vn/twitterclient/SetupActivity.java
@@ -2,7 +2,9 @@ package usth.edu.vn.twitterclient;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -16,6 +18,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
 
@@ -28,9 +35,11 @@ public class SetupActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference userRef;
     private ProgressDialog loadingBar;
+    private StorageReference userProfileImageRef;
 
 
     private CircleImageView profileImage;
+    final static  int GALLERY_PICK=1;
 
     String currentUserId;
 
@@ -42,6 +51,9 @@ public class SetupActivity extends AppCompatActivity {
         mAuth= FirebaseAuth.getInstance();
         currentUserId= mAuth.getCurrentUser().getUid();
         userRef= FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+        userProfileImageRef= FirebaseStorage.getInstance().getReference().child("Profile Images");
+
+
 
         userName = findViewById(R.id.setup_username);
         fullName = findViewById(R.id.setup_fullname);
@@ -58,6 +70,49 @@ public class SetupActivity extends AppCompatActivity {
             }
         });
 
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_PICK);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GALLERY_PICK && requestCode== RESULT_OK  && data!=null) {
+            Uri imageUri = data.getData();
+            //add a cropping function ,use the arthur hub library
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+
+        if(requestCode== CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(requestCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                StorageReference filePath = userProfileImageRef.child(currentUserId+".jpg");
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SetupActivity.this,"Profile Image  stored successfully...",Toast.LENGTH_SHORT).show();
+
+//                            final String downloadUrl = task.getResult().getDownloadUrl().toString();
+                        }
+                    }
+                });
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 
     private void saveAccountSetupInformation() {
