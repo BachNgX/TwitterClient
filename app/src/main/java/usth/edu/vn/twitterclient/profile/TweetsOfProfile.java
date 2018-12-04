6617b6f0f6879bcf09fde0a55c1c61a1cb7b50cf
BachNgX <34470659+BachNgX@users.noreply.github.com>
2018-12-04 11:11:24 +0700

    .

---
+++ app/src/main/java/usth/edu/vn/twitterclient/profile/TweetsOfProfile.java
@@ -0,0 +1,29 @@
+package usth.edu.vn.twitterclient.profile;
+
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.UserTimeline;
+
+import usth.edu.vn.twitterclient.R;
+
+public class TweetsOfProfile extends Fragment {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_tweets_of_profile);
+//
+//        final UserTimeline userTimeline = new UserTimeline.Builder()
+//                .screenName("twitterdev")
+//                .build();
+//        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+//                .setTimeline(userTimeline)
+//                .build();
+//        setListAdapter(adapter);
+//    }
+
+}
